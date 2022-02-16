5f0a1fcf5373e11f3658cb8e37457d82f4671cfb
TRK856 <53193704+TRK856@users.noreply.github.com>
2022-02-16 14:33:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+document.getElementById("Calculate-Formula").addEventListener("click", MissingSide);
+
+function MissingSide() {
+    let FormulaPartA = +document.getElementById("FormulaPartA").value;
+    let FormulaPartB = +document.getElementById("FormulaPartB").value;
+    let FormulaPartC = +document.getElementById("FormulaPartC").value;
+
+    let MissingSide = 0;
+    if(FormulaPartA == 0){
+        MissingSide = Math.sqrt( (FormulaPartC ** 2) - (FormulaPartB ** 2));
+    }
+    if(FormulaPartB == 0){
+        MissingSide = Math.sqrt((FormulaPartC ** 2) - (FormulaPartA ** 2));
+    }
+    if(FormulaPartC == 0){
+        MissingSide = Math.sqrt((FormulaPartA ** 2) + (FormulaPartB ** 2));
+    }
+
+    document.getElementById("Missing-Side").innerHTML = MissingSide;
+    FeildsFilled();
+}
+
+// this is a work in progress
+// trying to create a system which detects if 2 feilds are FilledFormulaPartA
+
+function FeildsFilled() {
+    let FormulaPartA = +document.getElementById("FormulaPartA").value;
+    let FormulaPartB = +document.getElementById("FormulaPartB").value;
+    let FormulaPartC = +document.getElementById("FormulaPartC").value;
+
+    let FilledFeilds = 0;
+
+    if (FormulaPartA != 0){
+        FilledFeilds = FilledFeilds + 1;
+    } else {
+        FilledFeilds = FilledFeilds - 1;
+    }
+    if (FormulaPartB != 0){
+        FilledFeilds = FilledFeilds + 1;
+    } else {
+    FilledFeilds = FilledFeilds - 1;
+    }
+    if (FormulaPartC != 0){
+        FilledFeilds = FilledFeilds + 1;
+    } else {
+        FilledFeilds = FilledFeilds - 1;
+    }
+
+    if (document.getElementById("ERROR_form-feilds").innerHTML == "" && FilledFeilds != 1){
+        document.getElementById("ERROR_form-feilds").innerHTML = "*2 out of 3 feilds required*"
+        document.getElementById("Missing-Side").innerHTML = "ERROR:Feild_Missing";
+    } else if (document.getElementById("ERROR_form-feilds").innerHTML == "*2 out of 3 feilds required*" && FilledFeilds == 1){
+        document.getElementById("ERROR_form-feilds").innerHTML = ""
+    }
+}
